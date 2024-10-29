35b206b1ec3e43ad1c57481da2b568b1da856bdb
NANAME002 <dennzerogo@gmail.com>
2024-10-29 20:39:16 +0300

    Add files via upload

---
+++ Компьютерный практикум № 4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("a=  ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("b= ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("c= ");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a+b+c={a + b + c}");
+            //Console.ReadKey();
+
+            //Console.Write("side1=");
+            //double side1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("side2=");
+            //double side2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"S={side1}*{side2}={side1 * side2}");
+            //Console.WriteLine($"P=({side1}+{side2})*2={(side1 + side2) * 2}");
+            //Console.ReadKey();
+
+            //Console.Write("a= ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"с={a}*{a}+{b}*{b}={a * a + b * b}");
+            //Console.ReadKey();
+
+            //Console.Write("a= ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"P={a}*4={a * 4}");
+            //Console.WriteLine($"S={a}*{a}={a * a}");
+            //Console.ReadKey();
+
+        }
+    }
+}
